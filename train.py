930d408680297af92962a20c99db230540fec6db
Connor Anderson <connor.anderson@byu.edu>
2022-01-27 14:10:57 -0700

    Fix bug--missing epochs in steps calc

--- train.py
+++ train.py
@@ -17,12 +17,12 @@ MODEL_REGISTRY.register_classes(mae, mae.pl.LightningModule)
 # customize LightningCLI with some argument linking
 class CLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
-        # compute the number of training steps from the number of samples, batch size, and
-        # number of GPUs (used by the learning rate scheduler)
+        # compute the number of training steps from the number of samples, batch size,
+        # number of GPUs, and number of epochs (used by the learning rate scheduler)
         parser.link_arguments(
-            ('data.init_args.num_samples', 'data.init_args.batch_size', 'trainer.gpus'),
+            ('data.init_args.num_samples', 'data.init_args.batch_size', 'trainer.gpus', 'trainer.max_epochs'),
             'model.init_args.train_steps',
-            lambda a, b, c: a // b // c,
+            lambda a, b, c, d: (a // b // c) * d,
             'parse'
         )
 
