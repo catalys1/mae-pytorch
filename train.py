7ebefce717556d2070186507d9e7e3213ce9f014
matthias-brucklacher <matthias.brucklacher@gmail.com>
2024-01-12 17:15:13 +0100

    Fix setting of environment variable

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 from pytorch_lightning.cli import LightningCLI
 import torch
-
-from dotenv import load_dotenv
+import os
 
 
 if __name__ == '__main__':
     # load environment variables from ./.env
-    load_dotenv('.env')
+ 
+    os.environ["DATADIR"] = "/scratch-shared/matt1/data"
     torch.set_float32_matmul_precision('medium')
     cli = LightningCLI(parser_kwargs={'parser_mode': 'omegaconf'})
