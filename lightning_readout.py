75db974a1ab1a58f9d1c475431e8cc4c31ca26c4
matthias-brucklacher <matthias.brucklacher@gmail.com>
2024-01-15 14:19:29 +0100

    fix module import naming error

--- lightning_readout.py
+++ lightning_readout.py
@@ -1,5 +1,5 @@
 import math
-from datamodule import STL10DataModule
+from datamodule import STL10ReadoutDataModule
 import os
 from typing import Any, Tuple, Union
 from mae import MAE_linear_probing, MAE
@@ -9,7 +9,7 @@ import timm
 import torch
 from torch import distributed
 import torchvision
-# os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 # os.environ['TORCH_USE_CUDA_DSA'] = "1"
 # Load ViT model from ckpt
 ckpt_model = torch.load('last.ckpt')  # upload checkpoint to aws
@@ -17,7 +17,7 @@ model = MAE_linear_probing(ckpt_model)
 # model = MAE()
 
 # Prepare trainer with correct data splits
-stl10 = STL10DataModule(data_dir='../Datasets/stl10_binary', batch_size=1, num_workers=0, pin_memory=True, size=224, augment=True, num_samples=None)
+stl10 = STL10ReadoutDataModule(data_dir='/scratch-shared/matt1/data', batch_size=64, num_workers=17, pin_memory=True, size=224, augment=True, num_samples=None)
 trainer = Trainer()
 trainer.fit(model, datamodule=stl10)
 pass
