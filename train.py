a0034867ec11f72bead65d8a53b7101f350abf9b
matthias-brucklacher <matthias.brucklacher@gmail.com>
2024-01-28 11:05:18 +0100

    restore original handling of environment variables

--- train.py
+++ train.py
@@ -1,11 +1,9 @@
 from pytorch_lightning.cli import LightningCLI
 import torch
-import os
-
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    # load environment variables from ./.env
- 
-    os.environ["DATADIR"] = "/scratch-shared/matt1/data"
+
+    load_dotenv('.env')
     torch.set_float32_matmul_precision('medium')
     cli = LightningCLI(parser_kwargs={'parser_mode': 'omegaconf'})
